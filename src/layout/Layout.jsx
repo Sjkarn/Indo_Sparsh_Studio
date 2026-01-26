d3b5417623e7403507ca27d977b1ea40c34fd62e
Saurabh Karn <saurabhkarn007@gmail.com>
2026-01-26 12:41:55 +0530

    Done

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,28 +1,4 @@
-﻿// import { Outlet, Link } from "react-router-dom";
-
-// export default function Layout() {
-//   return (
-//     <div>
-//       <header style={{ padding: "15px", borderBottom: "1px solid #ccc" }}>
-//         <h2>Indo Sparsh Studio</h2>
-//         <nav>
-//           <Link to="/">Home</Link> |{" "}
-//           <Link to="/about">About</Link> |{" "}
-//           <Link to="/services">Services</Link> |{" "}
-//           <Link to="/blog">Blog</Link> |{" "}
-//           <Link to="/career">Career</Link> |{" "}
-//           <Link to="/contact">Contact</Link>
-//         </nav>
-//       </header>
-
-//       <main style={{ padding: "20px" }}>
-//         <Outlet />
-//       </main>
-//     </div>
-//   );
-// }
-
-import { Outlet, Link } from "react-router-dom";
+﻿import { Outlet, Link } from "react-router-dom";
 import { useState } from "react";
 import "./Layout.css";
 import AIChatWidget from "../pages/AIChatWidget";
